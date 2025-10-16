9c642c157e0310ebbf5b4a2283700393af42602e
Dương Thái Bảo <me@dtbao.io.vn>
2025-10-16 16:12:05 +0700

    feat(toast): toast popup

--- static_assets/js/instances/tab-message-handler.js
+++ static_assets/js/instances/tab-message-handler.js
@@ -1,27 +1,47 @@
 const messenger = new TabMessenger("scene");
 
+function toastMessage(message, type = "success", duration = 3000) {
+    const toast = document.createElement("div");
+    toast.className = `z-[120] fixed bottom-16 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded shadow-lg z-50 ${
+		type === "success" ? "bg-green-500" : "bg-red-500 text-white"
+	} `;
+    toast.textContent = message;
+    document.body.appendChild(toast);
+    console.log("Toast message:", message);
+    setTimeout(() => {
+        document.body.removeChild(toast);
+    }, duration);
+}
+
 messenger.start = function() {
-      // Nhận lệnh load scene
-    messenger.on("LOAD_SCENE", (payload) => {
-        loadScene(payload.data);
-    });
+	// Nhận lệnh load scene
+	messenger.on("LOAD_SCENE", (payload) => {
+		loadScene(payload.data);
+	});
 
-    // Báo READY về cho admin
-    messenger.send("SCENE_READY", { version: "1.0.0", fps: 60 });
+	// Báo READY về cho admin
+	messenger.send("SCENE_READY", { version: "1.0.0", fps: 60 });
 
-    // Load data
-    messenger.on("LOAD_DATA", (payload) => {
-        console.log("LOAD_DATA", payload);
-        if (payload.data) {
-            console.log("data load:", payload.data)
-            loadScene(payload.data.scene);
-            loadLocations(payload.data.locations || []);
-            loadHotspots(payload.data.hotspots || []);
-        }	
-    });
+	// Load data
+	messenger.on("LOAD_DATA", (payload) => {
+        toastMessage("Đang tải cảnh...", "success", 2000);
+		console.log("LOAD_DATA", payload);
+		if (payload.data) {
+			console.log("data load:", payload.data);
+			loadScene(payload.data.scene);
+			loadLocations(payload.data.locations || []);
+			loadHotspots(payload.data.hotspots || []);
+		}
+	});
 
-    // Báo khi đóng tab
-    window.addEventListener("beforeunload", () => {
-        messenger.send("SCENE_CLOSED");
-    });
+	// Load data
+	messenger.on("ACK_ACTION", (payload) => {
+        toastMessage(payload.message || "Action completed", payload.type, 4000);
+        messenger.ack = null;
+	});
+
+	// Báo khi đóng tab
+	window.addEventListener("beforeunload", () => {
+		messenger.send("SCENE_CLOSED");
+	});
 };
